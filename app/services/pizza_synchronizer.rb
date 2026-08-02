0a057761fdb554f1b8d03868c06dde773cde6a86
AlexSSY <adolf5341@gmail.com>
2026-08-02 23:02:33 +0000

    docs

--- app/services/pizza_synchronizer.rb
+++ app/services/pizza_synchronizer.rb
@@ -1,10 +1,13 @@
+# Keep consistent the price and fingerprint of a pizza when it is created or updated.
+# The price and fingerprint are calculated using the PizzaPriceService and PizzaFingerprint classes, respectively.
+# This service is called after a pizza is created or updated to ensure that the price and fingerprint are always up to date.
 class PizzaSynchronizer < ApplicationService
   param :pizza
   option :pizza_price_service_class, default: -> { PizzaPriceService }
   option :pizza_fingerprint_service_class, default: -> { PizzaFingerprint }
 
   def call
-    @pizza.price = pizza_price_service_class.new(@pizza).call
+    @pizza.price = pizza_price_service_class.new(pizza: @pizza).call
     @pizza.fingerprint = pizza_fingerprint_service_class.from(@pizza).call
     @pizza.save!
   end
